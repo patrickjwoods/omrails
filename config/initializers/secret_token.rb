c46420e9e0d2ff8c79dbf5e19690f7a25b5e92d2
Patrick Woods <patrickjwoods@gmail.com>
2013-08-17 23:28:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails::Application.config.secret_key_base = 'ba804b70422c720afce6fd3ecb1abc3b49ffbdcea9a62cc0cdcc90041f5420724ec34e135f078c933df091d722257b1090d2054f7c6ff0ab57561649ab4f4502'
