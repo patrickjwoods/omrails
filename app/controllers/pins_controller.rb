fe5bb68ccf6f38fb7a6dcb64e3e36a423119f4a6
Patrick Woods <patrickjwoods@gmail.com>
2013-08-22 13:21:30 -0500

    add paperclip gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -82,4 +82,9 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
+
+  def pin_params
+     params.require(:pin).permit(:description, :image)
+  end
+
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,17 @@
 class Pin < ActiveRecord::Base
-	validates :description, presence: true
-
+	
 	belongs_to :user
+	has_attached_file :image, styles: { medium: "320x240>"}
+
+
+	validates :description, presence: true
 	validates :user_id, presence: true
+	validates_attachment :image, presence: true, 
+								 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+								 size: { less_than: 5.megabytes }
+
+def pin_params
+   params.require(:pin).permit(:description, :image)
+end
 
 end
